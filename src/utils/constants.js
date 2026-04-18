def4d5af39b71f9e46820ae2e8d2956849d0b745
kuNA78 <tagoreacademy123@gmail.com>
2026-04-18 16:57:44 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,4 @@
-
-export const API_BASE = 'https://quizwebapp-s3g8.onrender.com';
+export const API_BASE = 'https://quizwebapp-s3g8.onrender.com/api';
 // export const API_BASE = import.meta.env.VITE_API_URL || '/api';
 export const TOKEN_KEY = 'token';
 export const USER_KEY = 'user';
