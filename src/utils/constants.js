d2b3ac9a6351a2a4c3547c5535e6e8301bc25dd4
kuNA78 <tagoreacademy123@gmail.com>
2026-04-18 15:15:30 +0530

    Update API_URL constant in constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://quizwebapp-s3g8.onrender.com'; // your real Render URL
+const API_URL = 'https://quizwebapp-s3g8.onrender.com'; 
 // const API_URL = import.meta.env.VITE_API_URL || '/api';
 export const TOKEN_KEY = 'token';
 export const USER_KEY = 'user';
