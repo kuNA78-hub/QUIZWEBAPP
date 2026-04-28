60008c85d24d77d3ed37f09e7abbaa5a6113366e
kuNA78 <tagoreacademy123@gmail.com>
2026-04-28 16:33:27 +0530

    Update resultRoutes.ts

--- server/routes/resultRoutes.ts
+++ server/routes/resultRoutes.ts
@@ -8,8 +8,9 @@ router.use(authenticateUser);
 
 router.get('/my-results', getUserResults);
 router.get('/leaderboard', getLeaderboard);
+router.get('/admin/all', isAdmin, getAllResults); 
 router.post('/', submitQuiz);
-router.get('/', isAdmin, getAllResults);
-router.get('/:id', getResultById);
+router.get('/', isAdmin, getAllResults);           // Alternate admin endpoint
+router.get('/:id', getResultById);                 // Parameterized route must be LAST
 
 export default router;
